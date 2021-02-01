ae320f0e7cc7894d8267bfb6ff4c86b6cf58a38b
Gurovich <gurovij@bk.ru>
2021-02-01 22:24:08 +0300

    3

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,24 @@
 package main;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            FileInputStream in = new FileInputStream("/media/gurovich/CCCOMA_X64FRE_RU-RU_DV9/test1");
+            int result;
+            List<Character> integers = new ArrayList<>();
+            while ((result = in.read()) != -1){
+                integers.add((char) result);
+            }
+            System.out.println(integers);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
